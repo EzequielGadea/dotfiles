1d16c8fe53a08772b1975d8ebd9fd1a7dc366ffa
Ezequiel Gadea <66884189+EzequielGadea@users.noreply.github.com>
2025-07-30 17:29:09 -0300

    Habilitar transparencia en nvim

--- nvim/.config/nvim/lua/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/plugins/rose-pine.lua
@@ -1,10 +1,18 @@
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
+    opt = {
+        transparency = true,
+    },
 	opts = {
 		variant = "main",
 	},
 	config = function()
+        require("rose-pine").setup({
+            styles = {
+                transparency = true,
+            }
+        })
 		vim.cmd("colorscheme rose-pine")
 	end,
 }
